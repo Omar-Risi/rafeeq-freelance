b51fe2796636fdd7f1c6e185df3c3e918421b565
reesi <=>
2025-10-24 23:48:06 +0400

    feat(add): added moderation page for client

---
+++ app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Resources\Clients\Schemas;
+
+use Filament\Forms;
+use Filament\Schemas\Components;
+use Filament\Schemas\Schema;
+
+class ClientForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Components\Fieldset::make('Contact information')
+                    ->columnSpanFull()
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->columnSpanFull()
+                            ->label("Client name")
+                            ->required(),
+                        Forms\Components\Radio::make('type')
+                            ->label("Type of client")
+                            ->columnSpanFull()
+                            ->options(["company", "individual"])
+                            ->inline(),
+                        Forms\Components\TextInput::make('email')
+                            ->label("Email"),
+                        Forms\Components\TextInput::make('phone_number')
+                            ->label("Phone Numebr")
+
+
+
+                    ])
+
+            ]);
+    }
+}
