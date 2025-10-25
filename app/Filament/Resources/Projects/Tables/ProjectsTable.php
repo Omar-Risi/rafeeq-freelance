ab72efd7f6f59be2d01d11cb9ec00d6fe8defcc8
reesi <=>
2025-10-25 10:32:27 +0400

    feat(add): added table

--- app/Filament/Resources/Projects/Tables/ProjectsTable.php
+++ app/Filament/Resources/Projects/Tables/ProjectsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Projects\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class ProjectsTable
@@ -13,7 +14,15 @@ class ProjectsTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('Project name'),
+                TextColumn::make('price')
+                    ->prefix('OMR ')
+                    ->label('Price')
+                    ->badge(),
+
+                TextColumn::make('client.name')
+                    ->label('Client name'),
             ])
             ->filters([
                 //
